a3f0636e7a445bdc912a5e8038a3760815db3aff
Velo <hartley.max@outlook.com>
2021-08-09 15:13:49 +0100

    add user model findbyemail component tests

--- server/tests/component/model/user.test.js
+++ server/tests/component/model/user.test.js
@@ -5,14 +5,14 @@ const User = require('../../../models/user');
 const db = require('../../../dbConfig/config');
 
 describe('User model', () => {
-	const testData = {
-		name: 'test user',
-		email: 'testuser@example.com',
-		password: 'test',
-	};
-	const { name, email, password } = testData;
-
 	describe('create', () => {
+		const testData = {
+			name: 'test user',
+			email: 'testuser@example.com',
+			password: 'test',
+		};
+		const { name, email, password } = testData;
+
 		beforeEach(async () => {
 			await resetTestDB();
 		});
@@ -60,6 +60,31 @@ describe('User model', () => {
 			try {
 				expect.assertions(1);
 				const data = await User.show('invalidId');
+			} catch (err) {
+				expect(err.message).toEqual('User not found');
+			}
+		});
+	});
+
+	describe('findByEmail', () => {
+		beforeEach(async () => {
+			await resetTestDB();
+		});
+
+		it('resolves with user', async () => {
+			expect.assertions(5);
+			const user = await User.findByEmail('testuser1@example.com');
+			expect(user).toBeInstanceOf(User);
+			expect(user.id).toEqual('d939bc6e-495d-457a-a997-aab91c4e080a');
+			expect(user.name).toEqual('test user 1');
+			expect(user.email).toEqual('testuser1@example.com');
+			expect(user.password).toEqual('$2a$04$1zezalVLhbbA.jBhdlMkm.JmR9eniEtFblJd3I3w05RUf0MmiLMh.');
+		});
+
+		it('rejects with error message for invalid email', async () => {
+			try {
+				expect.assertions(1);
+				const data = await User.findByEmail('invalid@example.com');
 			} catch (err) {
 				expect(err.message).toEqual('User not found');
 			}
