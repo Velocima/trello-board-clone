634d9a5a95db97ebcd9d1a2c6c0b35ead06ba1d9
Velo <hartley.max@outlook.com>
2021-08-09 11:17:53 +0100

    stub users controller unit test suite

---
+++ server/tests/unit/controller/users.test.js
@@ -0,0 +1,39 @@
+const User = require('../../../models/user');
+const usersController = require('../../../controllers/users');
+
+const mockSend = jest.fn();
+const mockJson = jest.fn();
+const mockStatus = jest.fn((code) => ({ send: mockSend, json: mockJson }));
+const mockRes = { status: mockStatus };
+
+describe('Users controller', () => {
+	let testUser = {
+		name: 'test user 1',
+		email: 'testuser1@example.com',
+		id: 'd939bc6e-495d-457a-a997-aab91c4e080a',
+	};
+
+	afterAll(() => {
+		jest.resetAllMocks();
+	});
+
+	describe('GET /user/:id', () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		it('returns a user with a 200 status code for valid id', async () => {
+			jest.spyOn(User, 'show').mockResolvedValueOnce(new User(testUser));
+			await usersController.show(null, mockRes);
+			expect(mockStatus).toHaveBeenCalledWith(200);
+			expect(mockSend).toHaveBeenCalledWith(testUser);
+		});
+
+		it('returns error message with a 404 status code for invalid id', async () => {
+			jest.spyOn(User, 'show').mockRejectedValueOnce(new Error('User not found'));
+			await usersController.show(null, mockRes);
+			expect(mockStatus).toHaveBeenCalledWith(404);
+			expect(mockSend).toHaveBeenCalledWith({ error: 'User not found' });
+		});
+	});
+});
