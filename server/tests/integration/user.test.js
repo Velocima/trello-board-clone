45493796f23713ec9680f82ff8ae26ee099305d3
Velo <hartley.max@outlook.com>
2021-08-09 10:24:34 +0100

    refactor integration describe blocks

--- server/tests/integration/user.test.js
+++ server/tests/integration/user.test.js
@@ -3,6 +3,12 @@ const { validate } = require('uuid');
 describe('User endpoints', () => {
 	let api;
 
+	let testUser = {
+		name: 'test user 1',
+		email: 'testuser1@example.com',
+		id: 'd939bc6e-495d-457a-a997-aab91c4e080a',
+	};
+
 	beforeAll(() => {
 		api = app.listen(3000, () => console.log('Test server running on port 3000'));
 	});
@@ -12,17 +18,12 @@ describe('User endpoints', () => {
 		await api.close();
 	});
 
-	describe('Success', () => {
-		let testUser = {
-			name: 'test user 1',
-			email: 'testuser1@example.com',
-			id: 'd939bc6e-495d-457a-a997-aab91c4e080a',
-		};
+	describe('GET /user/:id', () => {
 		beforeEach(async () => {
 			await resetTestDB();
 		});
 
-		it('GET to /user/:id should return status 200 with user information', (done) => {
+		it('should return status 200 with user information', (done) => {
 			request(api)
 				.get(`/users/${testUser.id}`)
 				.expect('Content-Type', /json/)
@@ -30,7 +31,17 @@ describe('User endpoints', () => {
 				.expect({ user: testUser }, done);
 		});
 
-		it('PUT to /user/:id should return status 200 user information', (done) => {
+		it('GET to user/:id should return 404 for invalid id', (done) => {
+			request(api).get('/users/1').expect(404).expect({ error: 'User not found' }, done);
+		});
+	});
+
+	describe('PUT /user/:id', () => {
+		beforeEach(async () => {
+			await resetTestDB();
+		});
+
+		it('should return status 200 user information', (done) => {
 			request(api)
 				.put(`/users/${testUser.id}`)
 				.send({ password: 'new password' })
@@ -39,12 +50,24 @@ describe('User endpoints', () => {
 				.expect(200)
 				.expect({ user: testUser }, done);
 		});
+	});
 
-		it('DELETE to /user/:id should return status 204', (done) => {
-			request(api).delete(`/users/${testUser.id}`).expect(204, done);
+	describe('DELETE /user/:id', () => {
+		beforeEach(async () => {
+			await resetTestDB();
 		});
 
-		it('POST to /user/register should return status 201 user information', (done) => {
+		it('should return status 204', (done) => {
+			request(api).delete(`/users/${testUser.id}`).expect(204, done);
+		});
+	});
+
+	describe('POST /user/register ', () => {
+		beforeEach(async () => {
+			await resetTestDB();
+		});
+
+		it('should return status 201 user information', (done) => {
 			const newUserData = {
 				name: 'new user',
 				password: 'superPassword',
@@ -64,8 +87,14 @@ describe('User endpoints', () => {
 				})
 				.expect({ user: { name, id: true, email } }, done);
 		});
+	});
 
-		it('POST to /user/login should return status 200', (done) => {
+	describe('POST /user/login ', () => {
+		beforeEach(async () => {
+			await resetTestDB();
+		});
+
+		it('should return status 200', (done) => {
 			request(api)
 				.post(`/users/login`)
 				.send({ email: testUser.email, password: 'test1' })
@@ -78,16 +107,6 @@ describe('User endpoints', () => {
 					}
 				})
 				.expect({ token: true, ok: true }, done);
-		});
-	});
-
-	describe('Error', () => {
-		beforeEach(async () => {
-			await resetTestDB();
-		});
-
-		it('GET to user/:id should return 404 for invalid id', (done) => {
-			request(api).get('/users/1').expect(404).expect({ error: 'User not found' }, done);
 		});
 	});
 });
