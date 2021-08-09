a4e94038b1623ae521cc514e2abcc53dd18c6bbd
Velo <hartley.max@outlook.com>
2021-08-09 16:07:29 +0100

    add user model findbyemail unit tests

--- server/tests/unit/model/user.test.js
+++ server/tests/unit/model/user.test.js
@@ -79,4 +79,39 @@ describe('User model', () => {
 			}
 		});
 	});
+
+	describe('findByEmail', () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		it('resolves with user on successful db query', async () => {
+			expect.assertions(5);
+			let userData = {
+				id: 'a3cb3416-8fcf-4719-8897-3f51767a578d',
+				name: 'test user',
+				password: 'test',
+				email: 'testuser@example.com',
+			};
+			jest.spyOn(db, 'query').mockResolvedValueOnce({
+				rows: [userData],
+			});
+			const user = await User.findByEmail(userData.id);
+			expect(user).toBeInstanceOf(User);
+			expect(user.id).toEqual(userData.id);
+			expect(user.name).toEqual(userData.name);
+			expect(user.email).toEqual(userData.email);
+			expect(user.password).toEqual(userData.password);
+		});
+
+		it('rejects with error message on invalid email', async () => {
+			try {
+				expect.assertions(1);
+				jest.spyOn(db, 'query').mockRejectedValueOnce({ rows: [] });
+				const data = await User.findByEmail('invalid@example.com');
+			} catch (err) {
+				expect(err.message).toEqual('User not found');
+			}
+		});
+	});
 });
