2a2f44c0ea6a71bf72acbc0c1683fcbe1238f8cf
Velo <hartley.max@outlook.com>
2021-08-12 17:51:42 +0100

    add user controller update function

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -17,4 +17,29 @@ async function show(req, res) {
 	}
 }
 
-module.exports = { show };
+async function update(req, res) {
+	try {
+		const { id } = req.params;
+		const { password } = req.body;
+		const user = await User.show(id);
+		const isUpdated = user.update(password);
+		if (!isUpdated) {
+			throw new Error('Unable to update');
+		}
+		const response = {
+			user: {
+				id: user.id,
+				name: user.name,
+				email: user.email,
+			},
+		};
+		res.status(200).send(response);
+	} catch (err) {
+		if (err.message === 'User not found') {
+			res.status(404).send({ error: err.message });
+		}
+		res.status(400).send({ error: err.message });
+	}
+}
+
+module.exports = { show, update };
