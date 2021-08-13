1d5a7d53f714923443d906ec8f5122a29932a158
Velo <hartley.max@outlook.com>
2021-08-13 11:49:40 +0100

    create and mount token validation middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+async function validateToken(req, res, next) {
+	const header = req.header('Authorization');
+	console.log(req.headers);
+	if (header) {
+		const token = header.split(' ')[1];
+		await jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
+			if (err) {
+				console.log(err.message);
+				res.status(403).send({ error: err.message });
+			} else {
+				next();
+			}
+		});
+	} else {
+		res.status(403).send({ err: 'Missing token' });
+	}
+}
+module.exports = { validateToken };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 const usersRouter = require('express').Router();
 const usersController = require('../controllers/users');
+const { validateToken } = require('../middleware/auth');
 
-usersRouter.get('/:id', usersController.show);
+usersRouter.get('/:id', validateToken, usersController.show);
+usersRouter.put('/:id', validateToken, usersController.update);
+usersRouter.delete('/:id', validateToken, usersController.destroy);
 usersRouter.post('/register', usersController.create);
-usersRouter.put('/:id', usersController.update);
-usersRouter.delete('/:id', usersController.destroy);
 usersRouter.post('/login', usersController.login);
 
 module.exports = usersRouter;
